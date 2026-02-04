e7cbc9fe76153e6d4ae945d4a476247d1581b1d9
Han <readesangjihan@gmail.com>
2026-02-04 03:30:31 -0500

    week0: add CubeIDE project + blink

--- firmware/sensor_logger/Core/Src/main.c
+++ firmware/sensor_logger/Core/Src/main.c
@@ -98,7 +98,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	  HAL_Delay(200);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
